636b2b15f1665e09964139eab7c91b177b813dc6
Deleplanque Matheo <matheo_deleplanque@ens.univ-artois.fr>
2024-09-22 00:51:55 +0200

    d

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -84,6 +84,20 @@ export default function Home() {
           </div>
         </div>
 
+        <div className="w-full lg:h-[400px] dark:bg-gray-900 rounded-xl flex flex-col lg:flex-row gap-12 p-8">
+          <h2 className="w-full lg:w-2/6 font-bold text-3xl text-center lg:-rotate-90">Mes formations</h2>
+          <div className="w-full lg:w-2/6 flex flex-col items-center gap-4 p-4 rounded-xl border-2 border-gray-400 dark:border-0  dark:bg-gray-700">
+            <img src="/iut.png" alt="Iut de Lens" className="w-5/6 h-44" />
+            <h2 className="text-xl font-bold">BUT Informatique</h2>
+            <p>Iut de Lens - 2023/2026</p>
+          </div>
+          <div className="w-full lg:w-2/6 flex flex-col items-center gap-4 p-4 rounded-xl border-2 border-gray-400 dark:border-0  dark:bg-gray-700">
+            <img src="/lycée.jpg" alt="Photo lycée henri darras" className="w-5/6 h-44" />
+            <h2 className="text-xl font-bold">BAC Gén. & Tech. (STI2D)</h2>
+            <p>Lycée Henri Darras - 2020/2023</p>
+          </div>
+        </div>
+
         <div className="w-full dark:bg-gray-900 rounded-xl flex flex-col gap-12 p-8">
           <h2 className="font-bold text-center text-3xl w-full">Mes compétences</h2>
           <div className="w-full flex flex-col lg:flex-row lg:flex-wrap items-center lg:items-normal justify-center gap-6">
@@ -107,7 +121,7 @@ export default function Home() {
 
         <div className="w-full flex flex-row gap-14 justify-center items-center border-2 border-gray-400 dark:border-0 dark:bg-gray-900 rounded-xl p-4">
           <a href="https://github.com/matheokcx" target="_blank"><img src="/github.png" alt="GitHub" className="w-16 h-16 transition-transform hover:scale-105" /></a>
-          <a href="https://www.linkedin.com/in/math%C3%A9o-deleplanque-4a4725329/" target="_blank"><img src="/linkedin.png" alt="GitHub" className="w-12 h-12 transition-transform hover:scale-105" /></a>
+          <a href="https://www.linkedin.com/in/math%C3%A9o-deleplanque-4a4725329/" target="_blank"><img src="/linkedin.png" alt="GitHub" className="w-14 h-14 transition-transform hover:scale-105" /></a>
           <a href="mailto:matheo.deleplanque@gmail.com" target="_blank"><img src="/mail.png" alt="Mail" className="w-16 h-16 transition-transform hover:scale-105" /></a>
         </div>
 
