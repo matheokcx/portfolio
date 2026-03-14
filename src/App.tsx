db60f4182d7d504241f2139f9b9ee7c2ee336c8f
Matheo <matheo.deleplanque@gmail.com>
2026-03-15 00:32:19 +0100

    refactor: Give up the 3D background for the moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import {lazy, Suspense} from "react";
 import HeroSection from "./components/Layout/HeroSection/HeroSection.tsx";
 import AboutSection from "./components/Layout/AboutSection/AboutSection.tsx";
 import SkillsSection from "./components/Layout/SkillsSection/SkillsSection.tsx";
 import ProjectsSection from "./components/Layout/ProjectsSection/ProjectsSection.tsx";
 import ExperiencesSection from "./components/Layout/ExperiencesSection/ExperiencesSection.tsx";
 
-const HeroBackground = lazy(() => import("./components/Layout/AnimatedBackground/AnimatedBackground.tsx"))
-
 const App = () => {
     return (
         <>
-            <Suspense fallback={null}>
-                <HeroBackground/>
-            </Suspense>
-
             <HeroSection/>
             <AboutSection/>
             <SkillsSection/>
